e0b49f39643d0cdf55e9363a3094f0b25e64d0c6
dkruijs-kvl <dkruijs@kvl.nl>
2019-08-22 16:52:18 +0200

    First commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Doing NLP analysis my twitter follows'' tweets for clustering/classification of my interests.',
+    author='Daan Kruijs',
+    license='',
+)
